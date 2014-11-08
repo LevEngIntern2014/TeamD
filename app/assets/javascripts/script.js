933695701c84fd2fbdc9c391ac5210c95bb6e9ca
chikam <c.coco.102@gmail.com>
2014-11-09 03:51:05 +0900

    あとはクリックしたときの処理

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -15,6 +15,20 @@ $(function() {
           $('#loadimg').html('');
           // 追加するHTMLを指定
           $('#summary').append('<div class="topic"><h2 class="title">notepad++で、PHPファイルの自動的終了htmlタグを有効にするにはどうしたらいいですか</h2><p class="pv">100pv</p>      <p class="question"><span>Question</span><br>notepad++を使用するときに、PHPファイルでhtmlタグを自動的に終了するにはどうすればいいでしょうか？notepad++を使用するときに、PHPファイルでhtmlタグを自動的に終了するにはどうすればいいでしょうか？htmlとxmlファイルでできるのですが(TextFXプラグインでできると思います)、PHPファイルだと上手くいきませんでした。PHPファイルで自動的終了タグを有効化するプラグイン(または組み込まれた機能)はありますか？</p><p class="answer"><span>Answer</span><br>少し面倒な作業になってしまうのですが、 最初はHTMLにして、終わってからPHPに切り替えるというのはどうでしょうか。</p><p class="answer"><span>Answer</span><br>少し面倒な作業になってしまうのですが </div>');
+  $(".question").hide();
+  $(".answer").hide();
+  var flg = "close";
+  $(".title").click(function(){
+    $(".pv").click().next(".question").next(".answer").click();
+  });
+  $(".pv").click(function() {
+    $(this).next(".question").slideToggle().next(".answer").slideToggle();
+      if(flg == "close"){
+        flg = "open";
+      }else{
+        flg = "close";
+      }
+  });
 
           // 処理が完了したら「loading」をfalseにする
           obj.data('loading', false);
