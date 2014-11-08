a76e6add60b7f68750433a012c48cba94aa5bbbc
tyoshida <tyoshida@mikilab.doshisha.ac.jp>
2014-11-08 19:45:47 +0900

    APIテスト

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,2 +1,12 @@
+require 'open-uri'
+require 'json'
+
 class HomePagesController < ApplicationController
+  def home    
+    html = open('https://teratail.com/api/questions?limit=100$sort=new').read
+    json = JSON.parser.new(html)
+    puts json.class
+    @result =  json.parse()
+  end
 end
+
