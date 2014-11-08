f784bf923499f56d4b15b51e2339289ebdf26963
chikam <c.coco.102@gmail.com>
2014-11-09 02:04:57 +0900

    pvを入れた

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,11 +2,14 @@ $(function() {
   $(".contents").hide();
   var flg = "close";
   $(".title").click(function(){
+    $(".pv").click();
+  });
+  $(".pv").click(function() {
     $(this).next(".contents").slideToggle();
       if(flg == "close"){
         flg = "open";
       }else{
         flg = "close";
       }
-    });
   });
+});
