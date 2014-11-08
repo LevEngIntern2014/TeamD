7b8901d9031cc666f414c66dd79805b32bf19379
chikam <c.coco.102@gmail.com>
2014-11-09 02:29:34 +0900

    pv,question,answerを作った

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,11 +1,12 @@
 $(function() {
-  $(".contents").hide();
+  $(".question").hide();
+  $(".answer").hide();
   var flg = "close";
   $(".title").click(function(){
-    $(".pv").click();
+    $(".pv").click().next("question").next(".answer").click();
   });
   $(".pv").click(function() {
-    $(this).next(".contents").slideToggle();
+    $(this).next(".question").slideToggle().next(".answer").slideToggle();
       if(flg == "close"){
         flg = "open";
       }else{
